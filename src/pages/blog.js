f38227dcce7e72a0c019771604bf3dbedaf7e308
jamesedchristie <jamesedchristie1@gmail.com>
2021-04-01 22:12:56 +1100

    Fix blog list display

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import { Date, RichText } from 'prismic-reactjs';
 
 import Layout from '../components/layout';
@@ -11,14 +11,26 @@ export default function Blog({ data }) {
         <h2>Blog Posts</h2>
         <br />
         {data.allPrismicBlogPost.edges.map(({ node }) => (
-          <div>
-            <h4>{RichText.asText(node.data.title.raw)}</h4>
-            <div>{Date(node.data.datetime).toString()}</div>
-            <div
-              dangerouslySetInnerHTML={{ __html: node.data.content.html }}
-            ></div>
-            <img src={node.data.image.url} height="50px" />
-          </div>
+          <Link to={`/blog/${node.uid}`}>
+            <article style={styles.article}>
+              <div style={styles.blogImage}>
+                <img src={node.data.image.fluid.src} />
+              </div>
+              <div style={styles.blogText}>
+                <div style={styles.titleRow}>
+                  <h3 style={styles.title}>{RichText.asText(node.data.title.raw)}</h3>
+                  <div style={styles.date}>
+                    {Date(node.data.datetime).toLocaleString()}
+                  </div>
+                </div>
+                <div style={styles.excerptRow}>
+                  <aside style={styles.excerpt}>
+                    {node.data.content.text.substring(0, 50)}
+                  </aside>
+                </div>
+              </div>
+            </article>
+          </Link>
         ))}
       </div>
     </Layout>
@@ -33,15 +45,16 @@ export const query = graphql`
           id
           data {
             title {
-              html
               raw
             }
             content {
-              html
+              text
             }
             datetime
             image {
-              url
+              fluid(maxWidth: 100) {
+                src
+              }
             }
           }
           uid
@@ -51,4 +64,41 @@ export const query = graphql`
   }
 `;
 
-const styles = {};
+const styles = {
+  article: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+  blogImage: {
+    flex: 1,
+  },
+  blogText: {
+    flex: 3,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  titleRow: {
+    flex: 1,
+    height: 'auto',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    flex: 1,
+    margin: 0
+  },
+  date: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  excerptRow: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  excerpt: {
+    
+  },
+};

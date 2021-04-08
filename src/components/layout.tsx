65f50481d5fa69de6dbacd6f31cf760fa327c3aa
jamesedchristie <jamesedchristie1@gmail.com>
2021-04-08 21:48:39 +1000

    Added active page prop and link styling

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,50 +1,66 @@
-import React from 'react';
+import React, { ReactNode, ReactPropTypes } from 'react';
 import { Link, PageProps } from 'gatsby';
 import '@fontsource/yantramanav';
-import '../global.css';
+import '../styles/global.css';
 import gatsbyLogo from '../images/gatsby.png';
 import githubLogo from '../images/github.png';
 import prismicLogo from '../images/prismic.png';
 import twitterLogo from '../images/twitter.svg';
 import CSS from 'csstype';
+import { bgColor, accentColor } from '../styles/colors';
 
-export default function Layout({ children }: { children: React.ReactNode}) {
+type LayoutProps = {
+  currentPage: string;
+  children: ReactNode;
+};
+
+const pages: string[] = ['About', 'Blog', 'Projects'];
+
+export default function Layout({ currentPage, children }: LayoutProps) {
   return (
-      <div style={bodyStyle}>
-        <header style={headerStyle}>
-          <h2>James Christie</h2>
-          <h3>Full Stack Developer</h3>
-          <nav style={navStyle}>
-            <Link style={navLinkStyle} to="/about/">
-              About
+    <div style={bodyStyle}>
+      <header style={headerStyle}>
+        <h2>James Christie</h2>
+        <h3>Full Stack Developer</h3>
+        <nav style={navStyle}>
+          {pages.map((p) => (
+            <Link style={p.toLowerCase() === currentPage ? navLinkActiveStyle : navLinkStyle } to={`/${p.toLowerCase()}/`}>
+              {p}
             </Link>
-            <Link style={navLinkStyle} to="/blog/">
-              Blog
-            </Link>
-            <Link style={navLinkStyle} to="/projects/">
-              Projects
-            </Link>
-          </nav>
-        </header>
-        <main>{children}</main>
-        <footer style={footerStyle}>
-          <p style={socialStyle}>
-            <Link
-              style={socialLinkStyle}
-              to="https://twitter.com/jamesedchristie"
-            >
-              <img src={twitterLogo} height="30px" />
-            </Link>
-            <Link
-              style={socialLinkStyle}
-              to="https://github.com/jamesedchristie"
-            >
-              <img src={githubLogo} height="30px" />
-            </Link>
-          </p>
-          <p style={footerTagStyle}><span>Made with </span><img src={gatsbyLogo} height="30px" style={{ margin: '0px 5px' }}/><span> Gatsby and </span><img src={prismicLogo} height="30px" style={{ margin: '0px 5px' }} /><span> Prismic</span></p>
-        </footer>
-      </div>
+          ))}
+          {/* <Link style={navLinkStyle} to="/about/">
+            About
+          </Link>
+          <Link style={navLinkStyle} to="/blog/">
+            Blog
+          </Link>
+          <Link style={navLinkStyle} to="/projects/">
+            Projects
+          </Link> */}
+        </nav>
+      </header>
+      <main>{children}</main>
+      <footer style={footerStyle}>
+        <p style={socialStyle}>
+          <Link
+            style={socialLinkStyle}
+            to="https://twitter.com/jamesedchristie"
+          >
+            <img src={twitterLogo} height="30px" />
+          </Link>
+          <Link style={socialLinkStyle} to="https://github.com/jamesedchristie">
+            <img src={githubLogo} height="30px" />
+          </Link>
+        </p>
+        <p style={footerTagStyle}>
+          <span>Made with </span>
+          <img src={gatsbyLogo} height="30px" style={{ margin: '0px 5px' }} />
+          <span> Gatsby and </span>
+          <img src={prismicLogo} height="30px" style={{ margin: '0px 5px' }} />
+          <span> Prismic</span>
+        </p>
+      </footer>
+    </div>
   );
 }
 
@@ -52,40 +68,48 @@ const bodyStyle: CSS.Properties = {
   fontFamily: 'Yantramanav',
 };
 const headerStyle: CSS.Properties = {
-  backgroundColor: 'beige',
+  backgroundColor: bgColor,
   textAlign: 'center',
   padding: '10px 10px',
+  borderBottom: '1px solid ' + accentColor
 };
 const navStyle: CSS.Properties = {
   display: 'flex',
   justifyContent: 'center',
-  marginTop: '50',
+  marginTop: '50px',
 };
 const navLinkStyle: CSS.Properties = {
   display: 'block',
   margin: '0px 20px',
   flex: 'initial',
   fontWeight: 'bold',
-  fontSize: '24',
+  fontSize: '24px',
   textDecoration: 'none',
   color: 'inherit',
+};
+const navLinkActiveStyle: CSS.Properties = {
+  display: 'block',
+  margin: '0px 20px',
+  flex: 'initial',
+  fontWeight: 'bold',
+  fontSize: '24px',
+  textDecoration: 'none',
+  color: 'grey',
 };
 const mainStyle: CSS.Properties = {
   margin: '3rem',
 };
 const footerStyle: CSS.Properties = {
-  backgroundColor: 'beige',
+  backgroundColor: bgColor,
   padding: '10px 0px',
-  position: 'fixed',
-  bottom: 0,
-  width: '100%'
+  width: '100%',
 };
 const socialStyle: CSS.Properties = {
   display: 'flex',
   flexDirection: 'row',
   justifyContent: 'center',
   margin: 0,
-  marginBottom: '5'
+  marginBottom: '5px',
 };
 const socialLinkStyle: CSS.Properties = {
   display: 'block',
@@ -99,5 +123,5 @@ const footerTagStyle: CSS.Properties = {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  margin: 0
+  margin: 0,
 };
